3138cbed2c85fd2ce17cb21b2365d54b9d260edd
Görkem Arslanboğan <99605700+gorkemarslanbogan@users.noreply.github.com>
2023-10-01 18:31:35 +0300

    Add files via upload
    My homeworks about kotlin

---
+++ Ödevler/odev_2/GORKEM_ARSLANBOGANODEV2/app/src/main/java/com/gorkemarslanbogan/gorkem_arslanboganodev2/OdevlerMain.kt
@@ -0,0 +1,67 @@
+package com.gorkemarslanbogan.gorkem_arslanboganodev2
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val dcOdevler = DcOdevler10Soru()
+
+//    println(dcOdevler.isPrime(21)) // başarılı
+//
+//    println(dcOdevler.Soru2(1,5)) // basarili
+//
+//    println(dcOdevler.Soru3(5,10)) //basarili
+
+//    println(dcOdevler.Soru4(555)) // basarili
+
+//    println(dcOdevler.palindromeSorusu(1223221)) //basarili
+
+
+     val odevler = Odevler2()
+
+//    println(odevler.isPrime(2)) //succes
+//    println(odevler.SayinintersiniYaz(2859)) //succes
+//    println(odevler.toplamHesapla(5, 10)) // review response of fun is mstake
+//    println(odevler.tekmimiCiftmi3Soru(5, 10)) //succes
+//    println(odevler.LongrakamlarToplami(105081)) //succes
+//    println(odevler.LongrakamlarToplami(105081)) //succes
+//    println(odevler.isPalindrom(1421)) //succes
+//    println(odevler.enBuyukEnKucukToplami(intArrayOf(10,20,30))) //succes
+//    println(odevler.DizininIcındeSayiVarmi(intArrayOf(10,20,30), 30)) //succes
+//    println(odevler.DizilerdekiOrtakElemanSayisi(intArrayOf(10,20,30), intArrayOf(10,20,45,151,30))) //succes
+//    println(odevler.VucutKitleEndeksiHesapla(100.0,1.94)) //succes
+//    println(odevler.EnBuyukSayiyiBul()) //succes
+//    println(odevler.Soru12(10,2)) //succes
+//    println(odevler.Soru13()) //succes
+//    println(odevler.Soru14(10,2)) //succes
+//    println(odevler.Soru15()) //succes
+//    println(odevler.Soru16()) //succes
+//    println(odevler.Soru17()) //succes
+//    println(odevler.Soru18()) //succes
+//    println(odevler.Soru19()) //succes
+//    println(odevler.Soru20()) //succes
+//   println(odevler.Soru21()) //succes
+//    println(odevler.Soru22()) //succes
+//    println(odevler.Soru23()) //succes
+//    println(odevler.Soru24()) //succes
+//    println(odevler.Soru25()) //succes
+//    println(odevler.Soru26()) //succes
+//    println(odevler.Soru27()) //succes
+//    println(odevler.Soru28()) //succes
+//    println(odevler.Soru29()) //succes
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
