1afa7c2f41937606b611b1f240c3d857788e530b
Nazlıcan <103425386+NazlicanTerliksiz@users.noreply.github.com>
2023-10-15 21:03:33 +0300

    Nazlıcan Terliksiz
    Nazlıcan Terliksiz

---
+++ Ödevler/odev_3/NazlıcanTerliksiz/app/src/main/java/com/nazlican/turkcellgelecegiyazanlarhomework3/Question1.kt
@@ -0,0 +1,61 @@
+package com.nazlican.turkcellgelecegiyazanlarhomework3
+
+//Notepad++ İle Bir Dosya Oluşturun
+//İçine 15 Tane Sayı Yazın (Arada Pozitif, Negatif Sayılarda Olsun)
+//
+//Uygulamanıza Bu Dosyanın Yolunu Vereceksiniz.Uygulamanız Ekrana
+//Dosyada Kaç Tane :
+//- Pozitif
+//- Negatif
+//- Tek
+//- Çift Sayı Olduğunu Bastıracak.
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val filePath = "C:\\Users\\nazli\\OneDrive\\Masaüstü\\TurkcellGelecegiYazanlarHomework3"
+    try {
+        val file = File(filePath)
+        val data = file.readLines()
+
+        val counts = getCounts(data)
+        printCounts(counts)
+
+    } catch (ex: Exception) {
+        println("Dosya Bulunamadi.")
+    }
+}
+
+fun getCounts(data: List<String>): Array<Int> {
+    var positiveNumber = 0
+    var negativeNumber = 0
+    var evenNumber = 0
+    var oddNumber = 0
+
+    for (item in data) {
+        val number = item.toInt()
+        if (number >= 0) {
+            positiveNumber++
+        }
+        if (number < 0) {
+            negativeNumber++
+        }
+        if (number % 2 == 0) {
+            evenNumber++
+        } else
+            oddNumber++
+    }
+
+    return arrayOf(positiveNumber, negativeNumber, evenNumber, oddNumber)
+}
+
+fun printCounts(counts: Array<Int>) {
+    println("Pozitif ${counts[0]}")
+    println("Negatif ${counts[1]}")
+    println("Cift ${counts[2]}")
+    println("Tek ${counts[3]}")
+}
+
+
+
+
