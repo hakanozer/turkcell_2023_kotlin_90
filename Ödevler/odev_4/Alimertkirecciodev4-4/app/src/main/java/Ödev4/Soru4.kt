eed7789ce0aa105c76e1bd595c5ef30190b72c52
Alimertkirecci <143407753+Alimertkirecci@users.noreply.github.com>
2023-10-30 17:27:48 +0300

    Update Soru4.kt

--- Ödevler/odev_4/Alimertkirecciodev4-4/app/src/main/java/Ödev4/Soru4.kt
+++ Ödevler/odev_4/Alimertkirecciodev4-4/app/src/main/java/Ödev4/Soru4.kt
@@ -1,10 +1,9 @@
 fun main(args: Array<String>){
-    var liste = listOf(1,2,3,4,5,6)
-    for(element in liste){
-        println(element)
-    }
-    println()
-    for(index in 0..liste.size-1 ){
-        println(liste[index])
+    val liste = listOf("Salatalık", "1", "Ali", "Kerim", "Üzüm")
+
+    println("Listedeki Elemanlar:")
+    
+    for (eleman in liste) {
+        println(eleman)
     }
 }
