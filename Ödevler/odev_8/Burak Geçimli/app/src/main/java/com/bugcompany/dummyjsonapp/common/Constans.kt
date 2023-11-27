072ac06520848f967d548a73c4f468549fb17938
Burak Geçimli <62941438+burakgecimli@users.noreply.github.com>
2023-11-27 04:53:19 +0300

    Delete Ödevler/odev_8/Burak Geçimli directory

--- Ödevler/odev_8/Burak Geçimli/app/src/main/java/com/bugcompany/dummyjsonapp/common/Constans.kt
+++
@@ -1,9 +0,0 @@
-package com.bugcompany.dummyjsonapp.common
-
-//Api endpointlerini temsil eden obje.
-object Constans {
-    const val BASE_URL = "https://dummyjson.com/"
-    const val LOGIN = "auth/login"
-    const val GET_10_LIMIT_PRODUCT = "products?limit=10"
-    const val SEARCH_PRODUCTS = "products/search"
-}
