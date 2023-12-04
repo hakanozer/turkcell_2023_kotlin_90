a6ab0983c05c5951f96212f80dbabc721b7eba6b
Nazlıcan <103425386+NazlicanTerliksiz@users.noreply.github.com>
2023-12-04 14:06:54 +0300

    Delete Ödevler/odev_8/Nazlıcan Terliksiz directory

--- Ödevler/odev_8/Nazlıcan Terliksiz/app/src/main/java/com/nazlican/turkcellgelecegiyazanlarhomework8/data/model/ProductResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.nazlican.turkcellgelecegiyazanlarhomework8.data.model
-
-data class ProductResponse(
-    val limit: Int,
-    val products: List<Product>,
-    val skip: Int,
-    val total: Int
-)
--- Ödevler/odev_8/Nazlıcan Terliksiz/app/src/main/java/com/nazlican/turkcellgelecegiyazanlarhomework8/util/Constant.kt
+++
@@ -1,5 +0,0 @@
-package com.nazlican.turkcellgelecegiyazanlarhomework8.util
-
-object Constant {
-    const val BASE_URL = "https://dummyjson.com/"
-}
